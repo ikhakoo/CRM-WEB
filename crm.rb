381b377e2826dc1ea47ed364a19f12f06b7fc5a5
ikhakoo <ikhakoo@gmail.com>
2015-04-08 11:30:38 -0400

    added contact class

--- crm.rb
+++ crm.rb
@@ -1,6 +1,13 @@
+require_relative 'contact'
 require 'sinatra'
 
 get '/' do
-  @crm_app_name = "My CRM"
+  @crm_app_name = "Imran\'s CRM"
   erb :index
 end
+
+get '/contacts' do
+	@crm_app_name = "Imran\'s CRM"
+	erb :contacts
+end
+
