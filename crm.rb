147bb7af6a60f7644780e247320f180d60670b4e
ikhakoo <ikhakoo@gmail.com>
2015-04-10 11:54:02 -0400

    step 2 editing works

--- crm.rb
+++ crm.rb
@@ -20,21 +20,16 @@ get '/contacts' do
   erb :contacts
 end
 
-get "/contacts/1000" do
-  @contact = @@rolodex.find(1000)
-  erb :show_contact
-end
-
 get '/contacts/new' do
-	@crm_app_name = "Imran\'s CRM"
+  @crm_app_name = "Imran\'s CRM"
   @crm_current_time = current_time.getlocal
   erb :new_contact
 end
 
 get '/contacts/delete' do
-	@crm_app_name = "Imran\'s CRM"
+  @crm_app_name = "Imran\'s CRM"
   @crm_current_time = current_time.getlocal
-	erb :delete
+  erb :delete
 end
 
 post '/contacts' do
@@ -42,3 +37,36 @@ post '/contacts' do
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
+
+get "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get "/contacts/:id/edit" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
