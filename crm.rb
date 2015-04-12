fcdc416806b378024ad4edb12d142434c123f7d8
ikhakoo <ikhakoo@gmail.com>
2015-04-12 00:59:41 -0400

    added inventory feature

--- crm.rb
+++ crm.rb
@@ -17,7 +17,21 @@ class Contact
   property :country, String
   property :email, String
   property :note, String
-end 
+end
+
+class Inventory
+  include DataMapper::Resource
+
+  property :item_id, Serial
+  property :item_brand, String
+  property :item_type, String
+  property :item_color, String
+  property :item_cost, Float
+  property :item_quantity, Integer
+  property :item_sales_price, Float
+  property :item_sales_tax, Float
+  property :item_note, String
+ end  
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
@@ -114,6 +128,94 @@ delete '/contacts/:id' do
   if @contact
     @contact.destroy
     redirect to '/contacts'
+  else
+    raise Sinatra:NotFound
+  end
+end
+
+#### Inventory ####
+
+get "/inventory" do
+  @inventory = Inventory.all
+  puts @inventory.inspect
+  erb :inventory
+end
+
+get '/inventory/new' do
+  erb :new_inventory  
+end
+
+post "/inventory/new" do
+  puts "i've posted these from the form: #{params.inspect}"
+  inventory = Inventory.create(
+    :item_brand => params[:item_brand],
+    :item_type => params[:item_type],
+    :item_color => params[:item_color],
+    :item_quantity => params[:item_quantity],
+    :item_cost => params[:item_cost],
+    :item_sales_price => params[:item_sales_price],
+    :item_sales_tax => params[:item_sales_tax],
+    :item_note => params[:item_note]
+  )
+  redirect to('/inventory')
+end
+
+get "/inventory/:item_id" do
+  @inventory = Inventory.get(params[:item_id].to_i)
+  if @inventory
+    erb :show_inventory
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put "/inventory/:item_id" do
+  @inventory = Inventory.get(params[:id].to_i)
+  if @inventory
+    @inventory.item_brand = params[:item_brand]
+    @inventory.item_type = params[:item_type]
+    @inventory.item_color = params[:item_color]
+    @inventory.item_quantity = params[:item_quantity]
+    @inventory.item_cost = params[:item_cost]
+    @inventory.item_sales_price = params[:item_sales_price]
+    @inventory.item_sales_tax = params[:item_sales_tax]
+    @inventory.item_note = params[:item_note]
+    redirect to("/inventory")
+  else
+    raise Sinatra::NotFound
+  end
+end
+#edit contact
+get "/inventory/:item_id/edit" do
+  @inventory = Inventory.get(params[:item_id].to_i)
+  if @inventory
+    erb :edit_inventory
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+post "/inventory/:item_id/edit" do
+  puts "i've posted these from the form: #{params.inspect}"
+  @inventory = Inventory.get(params[:id].to_i)
+  inventory = Inventory.update(
+    :item_brand => params[:item_brand],
+    :item_type => params[:item_type],
+    :item_color => params[:item_color],
+    :item_quantity => params[:item_quantity],
+    :item_cost => params[:item_cost],
+    :item_sales_price => params[:item_sales_price],
+    :item_sales_tax => params[:item_sales_tax],
+    :item_note => params[:item_note]
+    )
+end
+
+#delete inventory
+delete '/inventory/:item_id' do
+  @inventory = Inventory.get(params[:item_id].to_i)
+  if @inventory
+    @inventory.destroy
+    redirect to '/inventory'
   else
     raise Sinatra:NotFound
   end
